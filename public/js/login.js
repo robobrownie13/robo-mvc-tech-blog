1272128ef54d7906f3eaa48cf8ffe5101cbb0c76
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2023-05-26 12:30:46 -0500

    added login information and set server and package.json

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+}
+
+const email = document.querySelector.('').value.trim();
+const password = document.querySelector.('').value.trim();
+
+if (email && password) {
+    const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({email, password}),
+        headers: {'Content-Type': 'application/json'},
+    });
+
+    console.log(response);
+
+    if(response.ok) {
+        document.location.replace('/');
+    } else{
+        alert('Failed to log in');
+    }
+    }
+};
+
+document
+  .querySelector('')
+  .addEventListener('submit', loginFormHandler);
