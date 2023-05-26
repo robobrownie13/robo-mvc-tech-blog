0b3a6d3a9ad7577484f4b91c4271db1e1f4914e2
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2023-05-26 13:36:39 -0500

    expanded on public js and seeds and server while adding controllers db models and seeds

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,26 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-}
+  event.preventDefault();
 
-const email = document.querySelector.('').value.trim();
-const password = document.querySelector.('').value.trim();
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-if (email && password) {
-    const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({email, password}),
-        headers: {'Content-Type': 'application/json'},
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
     });
 
     console.log(response);
 
-    if(response.ok) {
-        document.location.replace('/');
-    } else{
-        alert('Failed to log in');
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in");
     }
-    }
+  }
 };
 
 document
-  .querySelector('')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector("login-button")
+  .addEventListener("submit", loginFormHandler);
--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -17,7 +26,7 @@ const sess = {
     db: sequelize,
   }),
 };
-const session = require("express-session");
+
 app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
