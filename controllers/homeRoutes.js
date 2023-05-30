460510bcadc2ac89d0e9d11af5733608369fcc01
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2023-05-30 14:48:20 -0500

    Filled in controllers and editted views and utils

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,85 @@
+const router = require('express').Router();
+const Blogpost = require('../models/Blogpost');
+const User = require('../models/User');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const blogpostData = await Blogpost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blogposts = blogpostData.map((blogpost) =>
+      blogpost.get({ plain: true })
+    );
+
+    // Pass serialized data and session flag into template
+    res.render('home', {
+      blogposts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/blogpost/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    res.render('blogpost', {
+      ...project,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Blogpost }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
---
+++ public/js/dashboard.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#blogpost-title').value.trim();
+  const body = document.querySelector('#project-desc').value.trim();
+
+  if (title && body) {
+    const response = await fetch(`/api/blogposts`, {
+      method: 'POST',
+      body: JSON.stringify({ title, body }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create blogpost');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/blogposts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete blogpost');
+    }
+  }
+};
+
+document
+  .querySelector('.new-blogpost-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.blogpost-list')
+  .addEventListener('click', delButtonHandler);
